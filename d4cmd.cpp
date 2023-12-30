a47c89f4612b6640f467a6808a8ac3dd824bbacc
Danin Sadžak <daninsadzak@gmail.com>
2023-12-30 17:27:15 +0100

    command "quit" implemented

--- d4cmd.cpp
+++ d4cmd.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <filesystem>
+#include <string>
+
+namespace fs = std::filesystem;
 
 int main() {
-    std::cout << "Hello, World!\n";
+    std::string command;
+
+    while (true) {
+        std::cout << fs::current_path() << '\n';
+        std::getline(std::cin, command);
+
+        if (command == "quit") break;
+
+    }
     return 0;
 }

6561ce5dbd8b38ce01a02a9ed7f77debdcf3a325
Danin Sadžak <daninsadzak@gmail.com>
2023-12-30 17:49:39 +0100

    separated command name and command instruction

--- d4cmd.cpp
+++ d4cmd.cpp
@@ -6,9 +6,12 @@ namespace fs = std::filesystem;
 
 int main() {
     std::string command;
+    std::string path = "";
 
     while (true) {
-        std::cout << fs::current_path() << '\n';
+        path = fs::current_path().string();
+        
+        beginning:std::cout << path << ">";
         std::getline(std::cin, command);
 
         for (char& c : command) {
@@ -17,6 +20,11 @@ int main() {
 
         if (command == "quit") break;
 
+        std::size_t whitespacePosition = command.find(" ");
+        std::string commandName = command.substr(0, whitespacePosition);
+        std::string instruction = command.substr(whitespacePosition + 1);
+
+        
     }
     return 0;
 }

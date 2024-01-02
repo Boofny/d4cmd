ff951af592681674b1bdcc9b983036ae43e691f2
Danin Sadžak <daninsadzak@gmail.com>
2024-01-02 01:01:51 +0100

    command "writeto" - forgot to append text; it would overwrite what you previously wrote

--- d4cmd.cpp
+++ d4cmd.cpp
@@ -93,8 +93,10 @@ int main() {
             std::string messagePosition = command.substr(command.find(" ", whitespacePosition + 1));
             messagePosition = messagePosition.substr(1);
             std::string fileName = instruction.substr(0, whitespacePosition + 1);
+
+            std::cout << messagePosition << '\n';
             
-            std::ofstream file(fileName);
+            std::ofstream file(fileName, std::ios::app);
 
             if (file.is_open()) {
                 file << messagePosition << '\n';
